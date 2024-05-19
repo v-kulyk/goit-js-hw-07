041321f4a9ab59551abd61ecb56362397d38b7ed
Volodymyr Kulyk <vlad.dev.zp@gmail.com>
2024-05-19 13:35:49 +0300

    implement task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,14 @@
+const formEl = document.querySelector(".login-form");
+formEl.addEventListener("submit", onFormSubmit);
+function onFormSubmit(event) {
+  event.preventDefault();
 
+  const { email, password } = event.currentTarget.elements;
+
+  if (email.value === "" || password.value === "") {
+    alert("All form fields must be filled in");
+  } else {
+    console.log({ email: email.value, password: password.value });
+    event.currentTarget.reset();
+  }
+}
