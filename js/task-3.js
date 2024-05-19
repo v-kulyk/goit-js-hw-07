49b29be501e544e8396f37457185f5ab78fe59e6
Volodymyr Kulyk <vlad.dev.zp@gmail.com>
2024-05-19 13:12:44 +0300

    implement task-3

---
+++ js/task-3.js
@@ -0,0 +1,14 @@
+const inputEl = document.querySelector('input#name-input');
+
+inputEl.addEventListener('input', onInputChange);
+
+function onInputChange(event) {
+    const value = event.currentTarget.value.trim();
+    if (value === '') {
+        outputEl.textContent = 'Anonymous';
+    } else {
+        outputEl.textContent = value       
+    }
+}
+
+const outputEl = document.querySelector('span#name-output');
